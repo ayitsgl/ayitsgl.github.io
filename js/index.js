7385b76104c17ebe22a86912aac20aa1a896f8fa
sgl <151748309+ayitsgl@users.noreply.github.com>
2024-03-17 15:39:12 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ $("#login-button").click(function (event) {
   var userName = document.getElementById("userName").value;
   var pwd = document.getElementById("pwd").value;
   //修改密码请改此处
-  if (userName == "bgg" && pwd == "1011") {
+  if (userName == "wyjx" && pwd == "317") {
     event.preventDefault();
     $("form").fadeOut(500);
     $(".wrapper").addClass("form-success");
